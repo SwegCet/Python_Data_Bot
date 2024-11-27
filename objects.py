c1853b62c78f0ffb85ac0d92f627fed2601ac577
SwegCet <ethantruong0804@gmail.com>
2024-11-26 17:08:59 -0800

    Added classes and objects for the specified data

---
+++ objects.py
@@ -0,0 +1,26 @@
+#Defines 2d coordinates
+class Point: 
+    def __init__(self, x ,y):
+        self.x = x
+        self.y = y
+    
+class ImageBounds:
+    def __init__(self, pivot, width, height):
+        self.x = pivot.x
+        self.y = pivot.y
+        self.width = width
+        self.height = height
+    
+# Class for objects that will be tracked
+class PlayersStats:
+    def __init__(self, playerId, playerName, highestPower, currentPower, merits, victories, defeats, unitsKilled, unitsDead, unitsHealed):
+        self.playerId = playerId
+        self.playerName = playerName
+        self.highestPower = highestPower
+        self.currentPower = currentPower
+        self.merits = merits
+        self.victories = victories
+        self.defeats = defeats
+        self.unitsKilled = unitsKilled
+        self.unitsKilled = unitsDead
+        self.unitsHealed = unitsHealed
