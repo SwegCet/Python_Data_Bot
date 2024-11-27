3414e8a1913e11149e040f31d6cd61b6bd0e5c13
SwegCet <ethantruong0804@gmail.com>
2024-11-27 00:42:00 -0800

    Revamped approach to image recognition

--- memberScreenBot.py
+++ memberScreenBot.py
@@ -1,79 +1,79 @@
-import pyautogui
+import logging
 import time
 import os
+import pyautogui
 import pytesseract
 import cv2
 import numpy as np
+import uuid
 from PIL import Image
+from mouse import drag, moveTo, moveClickTarget, click
+from config import member_screen_coordinates
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-mouseInfo = pyautogui.mouseInfo()
+#mouseInfo = pyautogui.mouseInfo()
 
-#SIMPLIFYING THE CODE W FUNCTIONS
-def click(x, y, duration=1, interval=0.5, button="left"):
-    # Moves to a position and click
-    pyautogui.moveTo(x,y, duration)
-    pyautogui.click(interval=interval, button=button)
+def get_leader_screenshot(directory_path):
+    new_path = os.path.join(directory_path, str(uuid.uuid4()))
     
-def moveTo(x, y, duration=0.5):
-    pyautogui.moveTo(x,y, duration) 
-#This will move to the next rank (2-4)
-def moveRelative(x, y, duration = 1):
-#Move relative to the current position
-    pyautogui.moveRel(x,y, duration)
+    #open and close caching
+    cell_position = member_screen_coordinates["leader_location"]
+    click(cell_position)
+    findImageClick("./rss/imfoButton.png")
+    time.sleep(1)
     
-# This scrolls up when we cleared ranks 1-4
-def drag(start_x, start_y, end_x, end_y, duration=1, button="left"):
-    time.sleep(0.5)
-    # Drags from one position to another holding left click
-    pyautogui.mouseDown(start_x, start_y, button=button)
-    pyautogui.moveTo(end_x, end_y, duration)
-    pyautogui.mouseDown(button="left")
-    pyautogui.mouseUp()
+    saveProfile(new_path)
     
-def take_screenshot(filename, region=None):
-    #Get current directory of script
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    #define subfolder path
-    subfolder = os.path.join(current_dir, "imgs")
-    
-    #sleep it for a second
-    time.sleep(0.5)
-    
-    # Takes a screenshot and saves it to the given filename
-    screenshot = pyautogui.screenshot(region=region)
-    # Player Card Region (ONLY THE PLAYER ID): 320, 450, 190, 50
-    #Player More info: 100, 275, 1690, 765
-    
-    #Save the screenshot in subfolder
-    file_path = os.path.join(subfolder, filename)
-    screenshot.save(file_path)
+    moveClickTarget(member_screen_coordinates["more_info_location"])
+    time.sleep(1)
+    moveClickTarget(member_screen_coordinates["back_button_location"])
+    moveClickTarget(member_screen_coordinates["back_button_location"])
 
-def tesseractRead():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    subfolder = os.path.join(current_dir, "imgs")
-    output_file = os.path.join(current_dir, "ocr_output.txt")
-    
-    #Open the output file in write mode
-    with open(output_file, "w", encoding="utf-8") as f:
-        for file_name in os.listdir(subfolder):
-            if file_name.endswith((".png", ".jpg", ".jpeg")):
-                file_path = os.path.join(subfolder, file_name)
-                
-                #Open image    
-                img = Image.open(file_path)
-                
-                #perform OCR
-                text = pytesseract.image_to_string(img)
-                
-                #Write output to the text file
-                f.write(f"=== OCR Output for {file_name} ===\n")
-                f.write(text + "\n\n")
-                print(f"OCR processed for: {file_name}")
+def findImageClick(reference_image_path, should_click=True, retry_count = 3, threshold = 0.8):
+    for i in range(0, retry_count):
+        #screenshot and save image
+        bluestacks_screenshot = pyautogui.screenshot()
+        bluestacks_screenshot.save('bluestacks_roi.png')
+        
+        #load up the screenshot
+        screenshot = cv2.imread('bluestacks_roi.png')
+        
+        #load up reference image
+        template = cv2.imread(reference_image_path)
+        
+        #perform template matching
+        result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+         # Check if a valid match is found based on the threshold
+        if max_val >= threshold:
+            # Get the coordinates of the top-left corner of the best match
+            top_left = max_loc
 
+            # Calculate the center of the reference image
+            h, w = template.shape[:2]
+            center_x = top_left[0] + w // 2
+            center_y = top_left[1] + h // 2
 
+            # Simulate a click at the center of the reference image
+            if should_click:
+                pyautogui.click(center_x, center_y)
+                time.sleep(.5)
+            return True
+        else:
+            logging.info(
+                f"No valid match found with confidence level below the threshold. Value: {max_val}, Threshold: {threshold}, Image:{reference_image_path},Count: {i}")
+            time.sleep(.5)
+
+    return False
+
+def saveProfile(directory_path):
+    try:
+        os.makedirs(directory_path)
+    finally:
+        # Capture a screenshot of the BlueStacks screen within the ROI
+        bluestacks_screenshot = pyautogui.screenshot()
+        bluestacks_screenshot.save(os.path.join(directory_path, "profile_summary.png"))
 #simulate leader from the alliance members
 '''
 time.sleep(2)
@@ -85,7 +85,3 @@ take_screenshot("moreInfo.png")
 click(40,80)
 click(40,80)
 '''
-
-# Need to figure out how to 1-4, scroll and repeat all the way till 200th member
-
-#Once we reach 200th member, we can end if I use OCR to check if duplicate names appear
