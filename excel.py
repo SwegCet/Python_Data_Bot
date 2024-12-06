93a88ae7f70e0ce7357c35a0507cf18b3f2acbfa
SwegCet <ethantruong0804@gmail.com>
2024-12-06 04:23:51 -0800

    Added victories and defeats into the dictionary

--- excel.py
+++ excel.py
@@ -3,14 +3,16 @@ import pandas as pd
 def savePlayerStats(playerStats, playerStatsFileName):
     #convert the list of objects to a dictionary
     data = {
-        "Name": [p.name for p in playerStats],
+        "Name": [p.playerName for p in playerStats],
         "PlayerId": [p.playerId for p in playerStats],
         "CurrentPower": [p.currentPower for p in playerStats],
         "HighestPower": [p.highestPower for p in playerStats],
+        "Victories": [p.victories for p in playerStats],
+        "Defeats": [p.defeats for p in playerStats],
         "Merits": [p.merits for p in playerStats],
-        "Kills": [p.kills for p in playerStats],
-        "Dead": [p.dead for p in playerStats],
-        "Healed": [p.healed for p in playerStats]
+        "Kills": [p.unitsKilled for p in playerStats],
+        "Dead": [p.unitsDead for p in playerStats],
+        "Healed": [p.unitsHealed for p in playerStats]
     }
     
     #Create Data Frame from the dictionary
@@ -20,6 +22,8 @@ def savePlayerStats(playerStats, playerStatsFileName):
     df["CurrentPower"] = df["CurrentPower"].apply(lambda x: "{:,.0f}".format(x))
     df["Merits"] = df["Merits"].apply(lambda x: "{:,.0f}".format(x))
     df["HighestPower"] = df["HighestPower"].apply(lambda x: "{:,.0f}".format(x))
+    df["Victories"] = df["Victories"].apply(lambda x: "{:,.0f}".format(x))
+    df["Defeats"] = df["Defeats"].apply(lambda x: "{:,.0f}".format(x))
     df["Kills"] = df["Kills"].apply(lambda x: "{:,.0f}".format(x))
     df["Dead"] = df["Dead"].apply(lambda x: "{:,.0f}".format(x))
     df["Healed"] = df["Healed"].apply(lambda x: "{:,.0f}".format(x))
