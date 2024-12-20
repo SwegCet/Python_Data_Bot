26f75e5ff21c63849b2984d1fc3f606f482ea847
SwegCet <ethantruong0804@gmail.com>
2024-12-20 01:50:13 -0800

    Changed name of diffExcel.py to fit usecase

---
+++ diffMain.py
@@ -0,0 +1,22 @@
+from excel import generateDiffReports, mergeReports
+
+
+if __name__ == '__main__':
+    
+    # USE THIS IF YOU'RE TRYING TO MERGE TWO ALLIANCES INTO 1 SHEET
+    '''
+    file1Path = "echo.xlsx"
+    file2Path = "ech1.xlsx"
+    
+    mergeReports(file1Path, file2Path)
+    '''
+    
+    
+    # USE THIS IF YOU'RE COMPARING START OF SEASON VS END OF SEASON STATS
+    '''
+    file1Path = "echo.xlsx"
+    file2Path = "pass.xlsx"
+    
+    generateDiffReports(file1Path, file2Path)
+    '''
+    
